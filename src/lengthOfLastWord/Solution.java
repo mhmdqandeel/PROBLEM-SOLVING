df4620e7b568f7361a347fe36142efce681ac862
mhmdqandeel <mohaqan2004@gmail.com>
2025-10-30 16:32:06 +0300

    add solution of lengthOfLastWord

---
+++ src/lengthOfLastWord/Solution.java
@@ -0,0 +1,18 @@
+package lengthOfLastWord;
+
+public class Solution {
+
+    public int lengthOfLastWord(String s) {
+        String trimStr = s.trim();
+
+        int length = 0;
+
+        for (int i = trimStr.length()-1; i > 0 ; i--) {
+            if (trimStr.charAt(i) == ' ') {
+                break;
+            }
+            length++;
+        }
+        return length;
+    }
+}
